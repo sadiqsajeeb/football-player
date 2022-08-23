4dd7fdf09d7d3fceb64dcbfd5776e0e09130e3c9
sadiqsajeeb <contact.sajeeb@gmail.com>
2022-08-23 22:48:53 +0600

    added

--- js/script.js
+++ js/script.js
@@ -86,3 +86,95 @@ for (item of arofObj) {
 
 //common function
 
+function findInputNumber(inputId) {
+  const inputField = document.getElementById(inputId);
+  const inputString = inputField.value;
+  const inputFieldNumber = parseFloat(inputString);
+  return inputFieldNumber;
+}
+
+function findElementValue(element) {
+  const inputField = document.getElementById(element);
+  const inputString = inputField.innerText;
+  const inputFieldNumber = parseFloat(inputString);
+  return inputFieldNumber;
+}
+
+function setElementValue(findElement, setValue) {
+  const element = document.getElementById(findElement);
+  element.innerText = setValue;
+}
+
+const btnclick = document.getElementsByClassName("btnClicked");
+const selectedView = document.getElementById("selectedView");
+
+for (btn of btnclick) {
+  btn.addEventListener("click", function (Event) {
+    const h4 = document.createElement("h4");
+    const li = document.createElement("li");
+    const playerName = Event.target.parentNode.children[0].innerText;
+    li.innerText = playerName;
+    h4.appendChild(li);
+
+    if (selectedView.children.length >= 5) {
+      alert("Please Stop creating Child dud😂");
+    } else {
+      Event.target.classList.add("disabled");
+      Event.target.style.backgroundColor = "#787A91";
+      Event.target.innerText = "Slected";
+      selectedView.appendChild(h4);
+    }
+  });
+}
+
+let storeAmount;
+
+document.getElementById("btnCalculate").addEventListener("click", function () {
+  const budgetAmount = findInputNumber("budgetAmount");
+  const playerQuantity = selectedView.children.length;
+  const clearInput = document.getElementById("budgetAmount");
+
+  if (isNaN(budgetAmount) != true && budgetAmount > 0) {
+    if (playerQuantity != 0) {
+      const totalPlayerCost = budgetAmount * playerQuantity;
+      storeAmount = totalPlayerCost;
+      setElementValue("playerExpenses", totalPlayerCost);
+      clearInput.value = "";
+    } else {
+      clearInput.value = "";
+      alert("Add some Player first");
+    }
+  } else {
+    clearInput.value = "";
+    alert("Please Input a Number grater than 0");
+  }
+});
+
+document
+  .getElementById("calculateTotal")
+  .addEventListener("click", function () {
+    const Manager = findInputNumber("Manager");
+    const coach = findInputNumber("coach");
+    const clearManagerInput = document.getElementById("Manager");
+    const clearcoachInput = document.getElementById("coach");
+
+
+
+    if (storeAmount != "undefine" && isNaN(storeAmount) != true) {
+      if (isNaN(Manager) != true && isNaN(coach) != true && Manager > 0 && coach > 0) {
+        const totalAmount = Manager + coach;
+        const grandTotal = totalAmount + storeAmount;
+        setElementValue("totalValue", grandTotal);
+        clearManagerInput.value = "";
+        clearcoachInput.value = "";
+      } else {
+        clearManagerInput.value = "";
+        clearcoachInput.value = "";
+        alert("Enter a number Graeter than 0");
+      }
+    } else {
+      alert("Check upper requirements");
+      clearManagerInput.value = "";
+      clearcoachInput.value = "";
+    }
+  });
